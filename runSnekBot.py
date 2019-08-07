659f82a655e3952534078aa55b178644922d4a91
Eagleheardt <andregueret@gmail.com>
2019-08-06 20:21:21 -0500

    testing OOP

--- runSnekBot.py
+++ runSnekBot.py
@@ -36,7 +36,6 @@ if __name__ == '__main__':
             data = kwargs['data']
             text = data['text']
             if data['user'] != USER_ID:
-                utils.CLIENT = data['web_client']
                 print(data)
                 cmd.EVAL(kwargs)
 
