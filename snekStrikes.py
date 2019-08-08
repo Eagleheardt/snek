aaa886986725ecf8a75cba7c7357856b4d940d75
Eagleheardt <andregueret@gmail.com>
2019-08-07 19:38:19 -0500

    adding  commands!

--- snekStrikes.py
+++ snekStrikes.py
@@ -43,4 +43,31 @@ class ExampleCommand(Command):
 
 publishedCommands.append(ExampleCommand())
 
-#########################################################
+#############################################################
+
+###########################
+###   Example Command   ###
+###########################
+
+class HelpCommand(Command):
+    def __init__(self):
+        super().__init__(
+            name = HelpCommand, 
+            response = words.textHelp,
+            actions = self.doSomething, 
+            triggers = ['help'],
+            description =\
+                """
+                    This is the help command.
+                    It gives a list of all the ways the public can interact with Snek.
+                    There are a few commands left off, both for brevity and to keep spam down.
+                """
+            )
+
+    def doSomething(self, payLoad):
+        inChannelResponse(payLoad['channel'],self.response)
+        return
+
+publishedCommands.append(HelpCommand())
+
+#############################################################
