706f08d181d6d3ba8d216c1fd7f2174c921ab6ef
Eagleheardt <andregueret@gmail.com>
2019-08-10 18:45:00 -0500

    track8

--- snekUtils.py
+++ snekUtils.py
@@ -50,6 +50,7 @@ def directResponse(aUser, response):
 
 def dateStripper(someText=''):
 	firstDigitIndex = re.search("\d", someText)
+	print("489")
 	print("date stripper {}".format(firstDigitIndex))
 	if firstDigitIndex:
 		return someText[firstDigitIndex.start()]
