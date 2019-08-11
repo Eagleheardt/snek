9248cbb49751e0e5f87cbf65392f3cb53878a3ec
Eagleheardt <andregueret@gmail.com>
2019-08-10 23:55:11 -0500

    why3

--- snekUtils.py
+++ snekUtils.py
@@ -51,7 +51,7 @@ def directResponse(aUser, response):
 def dateStripper(someText=''):
 	firstDigitIndex = re.search("\d", someText)
 	print('en number')
-	print(firstDigitIndex)
+	print(firstDigitIndex.Start)
 	print('en number')
 	print(someText[(int(firstDigitIndex.group())) + 1:])
 	if firstDigitIndex:
