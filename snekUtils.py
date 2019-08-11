a0b8bb9a66be343436cc2b101e24dec80350e425
Eagleheardt <andregueret@gmail.com>
2019-08-10 19:17:58 -0500

    seperation9.6

--- snekUtils.py
+++ snekUtils.py
@@ -54,8 +54,8 @@ def dateStripper(someText=''):
 	print("489")
 	print("date stripper {}".format(firstDigitIndex.group()))
 	if firstDigitIndex:
-		print(someText[firstDigitIndex.group()])
-		return someText[firstDigitIndex.group()]
+		print(someText[firstDigitIndex.group():])
+		return someText[firstDigitIndex.group():]
 	else:
 		return None
 
