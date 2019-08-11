e4b6f8eaa41607c9877123e33dae283fa7020f10
Eagleheardt <andregueret@gmail.com>
2019-08-10 23:20:18 -0500

    using conversion8

--- slackutils.py
+++ slackutils.py
@@ -43,7 +43,7 @@ def dateConverter(someDate=''):
     print("date congert")
 
     try:
-        print("1")
+        print(someDate)
         datetimeObject = dt.strptime(someDate, '%Y-%m-%d')
         print(datetimeObject.strftime("%m/%d/%Y"))
         return datetimeObject
