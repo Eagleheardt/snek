e8886c284328e0aec92f08ad64584ffc2151b22f
Eagleheardt <andregueret@gmail.com>
2019-08-12 22:45:49 -0500

    dolla dolla bill ya'll

--- databaseProvider.py
+++ databaseProvider.py
@@ -1,10 +1,11 @@
 import sqlite3
 import numpy as np
 
-__MAIN_CONNECTION = None
+__MAIN_CONNECTION = ''
 
 def setConnection(DATABASE):
 	__MAIN_CONNECTION = sqlite3.connect(DATABASE, check_same_thread=False)
+	print(__MAIN_CONNECTION)
 	SCHEMA()
 
 def SCHEMA():
