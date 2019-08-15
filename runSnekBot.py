e1e0786e6ccd7109b33922e5ca79c633c731f0ee
Eagleheardt <andregueret@gmail.com>
2019-08-15 17:42:20 -0500

    initial commit

--- runSnekBot.py
+++ runSnekBot.py
@@ -66,10 +66,10 @@ if __name__ == '__main__':
                 slackutils.CLIENT = kwargs['web_client']
                 cmd.EVAL(kwargs['data'])
 
-        # @RTMClient.run_on(event='presence_change')
-        # def handle(**kwargs):
-        #     print(kwargs)
-        #     ch.restart_snek()
+        @RTMClient.run_on(event='presence_change')
+        def handle(**kwargs):
+            print(kwargs)
+            #ch.restart_snek()
 
         rtm_client = RTMClient(token=SLACK_TOKEN)
         rtm_client.start()
