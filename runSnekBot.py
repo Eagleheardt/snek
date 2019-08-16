e4610020bdc5d0e62605731cd9a2d33fb12cfe6f
Andre Gueret <andre.gueret@ruralsource.com>
2019-08-16 10:07:47 -0500

    subscribe

--- runSnekBot.py
+++ runSnekBot.py
@@ -68,6 +68,7 @@ if __name__ == '__main__':
 
         rtm_client = RTMClient(token=SLACK_TOKEN)
         rtm_client.start()
+        slackutils.subscribe()
 
         @RTMClient.run_on(event='presence_change')
         def handle(**kwargs):
