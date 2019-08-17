3666314fd86a0a953f900ef5e0911525fcbb0cbd
Eagleheardt <andregueret@gmail.com>
2019-08-17 17:28:25 -0500

    more scope

--- runSnekBot.py
+++ runSnekBot.py
@@ -35,7 +35,7 @@ SLACK_TOKEN = de.getToken() # Bot's Slack token
 ###############################
 ###   End the slack token   ###
 ###############################
-
+s=sched.scheduler(time.time, time.sleep)
 
 # .----------------.  .----------------.  .----------------.  .-----------------.  
 # | .--------------. || .--------------. || .--------------. || .--------------. | 
@@ -56,13 +56,13 @@ if __name__ == '__main__':
         wc = WebClient(token=SLACK_TOKEN)
         print("after WC")
         print(wc)
-        s = sched.scheduler(time.time, time.sleep)
+        
         print("after s")
         s.enter(1,1,ph.checkStatus(wc))
         print("after enter")
 
         print("RUN")
-        sched.scheduler.run(self=s)
+        sched.scheduler.run()
  
         @RTMClient.run_on(event='message')
         def handle(**kwargs):
